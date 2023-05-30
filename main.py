22287f0ceb59007a0cfc16c3649f04068f3791bb
Kyu Lee <kwooler@gmail.com>
2023-05-30 00:32:40 -0700

    A77

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+def shuffle(num1, num2):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
 def main():
-    num1 = list(map(int, input().split()))
-    num2 = list(map(int, input().split()))
+    num1 = [1, 2, 3]
+    num2 = [4, 5, 6, 7]
+    ret = shuffle(num1, num2)
+    print(f'The return value is: {ret}')
 
-    N = len(num1)
-    M = len(num2)
-    main.shuffle = []
-    ##################################################
-    # Code your program here
-    ##################################################
-
-
+    num1 = [1, 2, 3, 4, 5]
+    num2 = [6, 7, 8]
+    ret = shuffle(num1, num2)
+    print(f'The return value is: {ret}')
 ##
 
 
--- main_test.py
+++ main_test.py
@@ -8,10 +8,9 @@ import math
 def test_main_1():
     captureout = io.StringIO()
     sys.stdout = captureout
-    datastr = '1 2 3\n4 5 6 7'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '1 2 3\n4 5 6 7'
+    # sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('captured ', captureout.getvalue())
     lines = captureout.getvalue().split('\n')
@@ -25,23 +24,19 @@ def test_main_1():
     # res = re.search(regex_string, main.evenlist)
     # assert res != none
     # print(res.group())
-    assert len(main.main.shuffle) == 7
-    assert main.main.shuffle[0] == 1
-    assert main.main.shuffle[1] == 4
-    assert main.main.shuffle[2] == 2
-    assert main.main.shuffle[3] == 5
-    assert main.main.shuffle[4] == 3
-    assert main.main.shuffle[5] == 6
-    assert main.main.shuffle[6] == 7
+    num1 = [1, 2, 3]
+    num2 = [4, 5, 6, 7]
+    ret = main.shuffle(num1, num2)
+    print(f'The return value is: {ret}')
+    assert ret == [1, 4, 2, 5, 3, 6, 7]
 
 
 def test_main_2():
     captureout = io.StringIO()
     sys.stdout = captureout
-    datastr = '1 2 3 4 5\n6 7 8'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '1 2 3 4 5\n6 7 8'
+    # sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('captured ', captureout.getvalue())
     lines = captureout.getvalue().split('\n')
@@ -55,10 +50,8 @@ def test_main_2():
     # res = re.search(regex_string, main.evenlist)
     # assert res != none
     # print(res.group())
-    assert len(main.main.shuffle) == 8
-    assert main.main.shuffle[0] == 1
-    assert main.main.shuffle[1] == 6
-    assert main.main.shuffle[2] == 2
-    assert main.main.shuffle[3] == 7
-    assert main.main.shuffle[6] == 4
-    assert main.main.shuffle[7] == 5
+    num1 = [1, 2, 3, 4, 5]
+    num2 = [6, 7, 8]
+    ret = main.shuffle(num1, num2)
+    print(f'The return value is: {ret}')
+    assert ret == [1, 6, 2, 7, 3, 8, 4, 5]
